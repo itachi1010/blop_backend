64c5e025c7d031ebfd8a066123387431ca746863
itachi1010 <onahben977@gmail.com>
2024-05-12 04:09:17 +0100

    added login

--- authentification/views.py
+++ authentification/views.py
@@ -4,7 +4,8 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 from rest_framework_simplejwt.tokens import RefreshToken
-
+from django.contrib.auth.models import User
+from rest_framework.permissions import AllowAny
 
 class HomeView(APIView):
      
@@ -27,3 +28,18 @@ class LogoutView(APIView):
             return Response(status=status.HTTP_205_RESET_CONTENT)
         except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+        
+class RegisterView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        if not username or not password:
+            return Response({"error": "Username and password are required"}, status=status.HTTP_400_BAD_REQUEST)
+
+        if User.objects.filter(username=username).exists():
+            return Response({"error": "Username is already in use"}, status=status.HTTP_409_CONFLICT)
+
+        user = User.objects.create_user(username=username, password=password)
+        return Response({"message": "User created successfully"}, status=status.HTTP_201_CREATED)
